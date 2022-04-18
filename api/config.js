fc337ecf803f7ba7cd5e14dc71c164ee832cbe51
peterphot <photinospeter@gmail.com>
2022-04-18 19:54:20 +1000

    Deleted file from repository only

--- api/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    db: {
-        connectionString: 'postgres://pp:H0OybHIZIyHzDLiceUCv3ac2fDj6qpjh@dpg-c9dmbi79re0sv3mnq1mg-a.singapore-postgres.render.com/usr2?ssl=true'
-    }
-}
-
-module.exports = config;
